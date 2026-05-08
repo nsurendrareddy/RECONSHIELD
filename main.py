c1856ab39e882eca43918a1120bd02db5e78a210
N Suresh <nsurendrareddy3@gmail.com>
2026-05-08 16:42:58 +0530

    Files

--- main.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Add the server directory to the Python path
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'server'))
-
-# Import the FastAPI app from server/main.py
-from main import app
